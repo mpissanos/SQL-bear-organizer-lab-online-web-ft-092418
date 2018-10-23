e586c6965970d2794788255025e4dc2f6ae991ff
Manny Pissanos <mpissano@gmail.com>
2018-10-23 17:31:54 +0000

    automatically backed up by learn

--- lib/insert.sql
+++ lib/insert.sql
@@ -1,15 +1,15 @@
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 10, "male", "brown", "mean", false);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Mr. Chocolate", 10, "male", "brown", "mean", 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 10, "male", "brown", "mean", false);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Rowdy", 10, "male", "brown", "mean", 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 10, "male", "brown", "mean", false);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Tabitha", 10, "male", "brown", "mean", 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 10, "male", "brown", "mean", false);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Sergeant Brown", 10, "male", "brown", "mean", 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 10, "male", "brown", "mean", false);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Melissa", 10, "male", "brown", "mean", 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 10, "male", "brown", "mean", false);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Grinch", 10, "male", "brown", "mean", 1);
 
-INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 10, "male", "brown", "mean", false);
+INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES ("Wendy", 10, "male", "brown", "mean", 1);
 
 INSERT INTO bears (name, age, gender, color, temperament, alive) VALUES (null, 10, "male", "brown", "mean", false);
